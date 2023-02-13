ef956a0d01c2d9fbd37f41e2ee4e3f58f57c3697
lauriekell <laurie@seaplusplus.co.uk>
2023-02-13 08:30:18 +0000

    clean

--- R/dd.R
+++ R/dd.R
@@ -1,5 +1,5 @@
-ddWt<-function(wt,biomass,bmsy,alpha=-0.2,min=0.5,max=1.5){
-  adj=(biomass%/%bmsy)^alpha
+ddWt<-function(wt,biomass,bref,alpha=-0.2,min=0.5,max=1.5){
+  adj=(biomass%/%bref)^alpha
   
   wt%*%qmin(qmax(adj,min),max)}
 
@@ -7,36 +7,24 @@ ddWt<-function(wt,biomass,bmsy,alpha=-0.2,min=0.5,max=1.5){
 ddMat<-function(wt,x) 
   1/(1+exp(-x[1]%*%(wt%-%x[2])))
 
-ddM<-function(wt, F, gamma=2.0753+0.9914*F, lambda=-0.0276-0.1088*F, refWt=310, refM=0.10, M1=0.05){
+ddM<-function(wt,par){
+    par["m1"]%*%(wt%^%par["m2"])}
   
-  ref=refM/(gamma%*%exp(lambda*(refWt/0.0003)^(1/3)))
+ddFn<-function(year,x,par,massFlag=TRUE,matFlag=TRUE,mFlag=TRUE){
   
-  exp(((wt/0.0001)^(1/3))%*%lambda)%*%gamma%*%ref+M1}
+  if (massFlag){
+    lsr=landings.wt(x)[,year]/stock.wt(x)[,year]
+    dsr=discards.wt(x)[,year]/stock.wt(x)[,year]
+    dsr[!is.finite(dsr)]=0
+    
+    stock.wt(   x)[,year]=ddWt(stock.wt(x)[,year],biomass(x)[,ac(an(year)-1)],par["bref"])
+    landings.wt(x)[,year]=stock.wt(x)[,year]*lsr
+    discards.wt(x)[,year]=stock.wt(x)[,year]*dsr}
+    
+  if (matFlag) 
+    mat(x)[,year]=ddMat(stock.wt(x)[,year],par[c("matk","w50")])
 
-ddFn<-function(year,x,par){
-  lsr=landings.wt(x)[,year]/stock.wt(x)[,year]
-  dsr=discards.wt(x)[,year]/stock.wt(x)[,year]
-  dsr[!is.finite(dsr)]=0
-  
-  stock.wt(   x)[,year]=ddWt(stock.wt(x)[,year],biomass(x)[,ac(an(year)-1)],par["bmsy"])
-  landings.wt(x)[,year]=stock.wt(x)[,year]*lsr
-  discards.wt(x)[,year]=stock.wt(x)[,year]*dsr
-  
-  mat(x)[,year]=ddMat(stock.wt(x)[,year],par[c("k","w50")])
-  m(  x)[,year]=par["m1"]%*%(stock.wt(x)[,year]%^%par["m2"])
-  
-  x}
-
-ddFn2<-function(x,par){
-  lsr=landings.wt(x)/stock.wt(x)
-  dsr=discards.wt(x)/stock.wt(x)
-  dsr[!is.finite(dsr)]=0
-  
-  stock.wt(   x)=ddWt(stock.wt(x),biomass(x),par["bmsy"],alpha=-0.25)
-  landings.wt(x)=stock.wt(x)*lsr
-  discards.wt(x)=stock.wt(x)*dsr
-  
-  mat(x)=ddMat(stock.wt(x),par[c("k","w50")])
-  m(  x)=par["m1"]%*%(stock.wt(x)%^%par["m2"])
+  if (mFlag)   
+    m(  x)[,year]=par["m1"]%*%(stock.wt(x)[,year]%^%par["m2"])
   
   x}
